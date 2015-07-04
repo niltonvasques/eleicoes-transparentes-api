1aeb9687a8c617dcd908a9fba638bebeb793cc77
Nilton Vasques <nilton.vasques@gmail.com>
2015-07-03 21:17:06 -0300

    Add findById em users

--- db/users.js
+++ db/users.js
@@ -16,3 +16,20 @@ exports.findByToken = function(token, fn) {
       }
   );
 };
+
+exports.findById = function(id, fn) {
+  database.connection.query(
+      "SELECT * FROM Usuario WHERE id = ?", 
+      [id], 
+      function (err, rows, fields){
+        if(err){
+          console.log("Something bad happens.");
+          return fn(null, null);
+        }
+        if(rows.length > 0){
+          return fn(null, rows[0]);
+        } 
+        return fn(null, null);
+      }
+  );
+};
